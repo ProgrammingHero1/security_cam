aa1a0fd9a3b3598a3047dd4aba3cd95a59219aa2
Programming Hero <53802153+ProgrammingHero1@users.noreply.github.com>
2021-01-02 14:32:26 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+import winsound
+cam = cv2.VideoCapture(1)
+while cam.isOpened():
+    ret, frame1 = cam.read()
+    ret, frame2 = cam.read()
+    diff = cv2.absdiff(frame1, frame2)
+    gray = cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+    dilated = cv2.dilate(thresh, None, iterations=3)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
+    for c in contours:
+        if cv2.contourArea(c) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        winsound.PlaySound('alert.wav', winsound.SND_ASYNC)
+    if cv2.waitKey(10) == ord('q'):
+        break
+    cv2.imshow('Granny Cam', frame1)
